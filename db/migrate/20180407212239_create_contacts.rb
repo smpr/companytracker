d64fccdbf388a5bed96a44087bb2f4cea2adf1b9
smpr <gcbrown1116@gmail.com>
2018-04-07 16:27:03 -0500

    models for company and contacts setup

---
+++ db/migrate/20180407212239_create_contacts.rb
@@ -0,0 +1,21 @@
+class CreateContacts < ActiveRecord::Migration[5.1]
+  def change
+    create_table :contacts do |t|
+      t.string :lName
+      t.string :fName
+      t.string :email
+      t.boolean :linkedIn
+      t.boolean :firstLetter
+      t.boolean :firstResponse
+      t.boolean :secondLetter
+      t.boolean :secondResponse
+      t.boolean :thirdLetter
+      t.boolean :thirdResponse
+      t.string :notes
+      t.string :interview
+      t.references :company, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
