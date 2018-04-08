1dd625188bad12e5fed765851472926d3b1a9986
smpr <gcbrown1116@gmail.com>
2018-04-07 23:14:54 -0500

    comop home now mapping through all companies

--- client/src/components/Company/CompHome.js
+++ client/src/components/Company/CompHome.js
@@ -1,11 +1,45 @@
 import React, { Component } from 'react';
-
+import axios from 'axios'
+import { Link } from 'react-router-dom'
+import { Container, FormContainer, BodyContainer, Style, TextLabelStyle } from "../StyledComponents/DefaultStyle"
+import TextField from 'material-ui/TextField';
+import RaisedButton from 'material-ui/RaisedButton';
 class CompHome extends Component {
+    state={
+        companies: []
+    }
+    async componentWillMount() {
+        try {
+
+            const company = await axios.get(`/api/companies`)
+            this.setState({ companies: company.data })
+            console.log(this.state.companies)
+
+        } catch (error) {
+            console.log(error)
+        }
+
+    }
     render() {
         return (
-            <div>
-                Company Home
-            </div>
+            <BodyContainer>
+                <Container>
+                    <Link to={`/Company/Create`}>Create a Company</Link>
+                    <FormContainer>
+                
+                <ul>
+                    {this.state.companies.map((company, index) => {
+                        return (
+                            <div>
+                                <Link to={`/Company/${company.id}`}>{company.name}</Link>
+                            </div>
+                        )
+                    })}
+                    
+                </ul>
+                </FormContainer>
+                </Container>
+            </BodyContainer>
         );
     }
 }
