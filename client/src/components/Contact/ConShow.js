e72d013df77dbe271e88d934b915cdbc704214e3
smpr <gcbrown1116@gmail.com>
2018-04-08 00:41:01 -0500

    style added to conshow

--- client/src/components/Contact/ConShow.js
+++ client/src/components/Contact/ConShow.js
@@ -1,11 +1,40 @@
 import React, { Component } from 'react';
 
+import axios from 'axios'
+import { Redirect, Link } from 'react-router-dom'
+import { Container, FormContainer, BodyContainer, Style, TextLabelStyle } from "../StyledComponents/DefaultStyle"
+import TextField from 'material-ui/TextField';
+import RaisedButton from 'material-ui/RaisedButton';
 class ConShow extends Component {
+    state = {
+        contact: {}
+    }
+    async componentWillMount() {
+        try {
+            const compId = this.props.match.params.compId
+            const conId = this.props.match.params.conId
+            const contact = await axios.get(`/api/companies/${compId}/contacts/${conId}`)
+            this.setState({ contact: contact.data })
+            console.log(this.state.contact)
+
+        } catch (error) {
+            console.log(error)
+        }
+
+    }
     render() {
         return (
-            <div>
-                Contact Show
-            </div>
+            <BodyContainer>
+                <Container>
+                    <div>
+                    Contact Info
+                    
+                        <FormContainer>
+                            Name: {this.state.contact.fName} {this.state.contact.lName}
+                        </FormContainer>
+                    </div>
+                </Container>
+            </BodyContainer>
         );
     }
 }
